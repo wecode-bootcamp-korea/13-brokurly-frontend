b0d01a800205efa1c23878a8745af95f97f0358f
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2020-10-19 22:28:04 +0900

    Add: Search Id/Pw

--- src/Pages/Login/Login.component.js
+++ src/Pages/Login/Login.component.js
@@ -19,38 +19,40 @@ class Login extends Component {
   render() {
     return (
       <div className="Login">
-        <h3 className="login-title">로그인</h3>
-        <form className="login-form">
-          <input
-            className="id-input"
-            name="userId"
-            type="text"
-            placeholder="아이디를 입력해주세요"
-            onChange={this.handleIdPwd}
-          ></input>
-          <input
-            className="pwd-input"
-            name="userPwd"
-            type="text"
-            placeholder="비밀번호를 입력해주세요"
-            onChange={this.handleIdPwd}
-          ></input>
-          <div className="check-and-search">
-            <div className="check">
-              <label htmlFor="connect-type">
-                <input type="checkbox" name="connect" id="connect-type" />
-                보안접속
-              </label>
+        <div className="login-container">
+          <h3 className="login-title">로그인</h3>
+          <form className="login-form">
+            <input
+              className="id-input"
+              name="userId"
+              type="text"
+              placeholder="아이디를 입력해주세요"
+              onChange={this.handleIdPwd}
+            />
+            <input
+              className="pwd-input"
+              name="userPwd"
+              type="text"
+              placeholder="비밀번호를 입력해주세요"
+              onChange={this.handleIdPwd}
+            />
+            <div className="check-and-search">
+              <div className="check">
+                <label htmlFor="connect-type">
+                  <input type="checkbox" name="connect" id="connect-type" />
+                  보안접속
+                </label>
+              </div>
+              <div className="search">
+                <Link className="search-id">아이디 찾기</Link>
+                <span className="bar">|</span>
+                <Link className="search-pwd">비밀번호 찾기</Link>
+              </div>
             </div>
-            <div className="search">
-              <Link className="search-id">아이디 찾기</Link>
-              <span className="bar">|</span>
-              <Link className="search-pwd">비밀번호 찾기</Link>
-            </div>
-          </div>
-          <button className="loginButton">로그인</button>
-          <Link className="moveSingup">회원가입</Link>
-        </form>
+            <button className="loginButton">로그인</button>
+            <Link className="moveSingup">회원가입</Link>
+          </form>
+        </div>
       </div>
     );
   }
--- src/Pages/Main/Main.component.js
+++ src/Pages/Main/Main.component.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import IdSearch from "../Login/IdSearch/IdSearch.component";
 import Login from "../Login/Login.component";
+import PwdSearch from "../Login/pwdSearch/PwdSearch.component";
 
 import "./Main.styles.scss";
 
 class Main extends Component {
   render() {
-    return <Login />;
+    return (
+      <Login />
+      // <IdSearch />
+      // <PwdSearch />
+    );
   }
 }
 
