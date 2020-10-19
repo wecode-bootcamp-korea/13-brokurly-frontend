88944f14f566c27c7ae55638e26ff29a83ce20f9
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2020-10-19 21:24:26 +0900

    Add: 로그인 페이지 기능 구현

--- src/Pages/Login/Login.component.js
+++ src/Pages/Login/Login.component.js
@@ -1,10 +1,58 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 import "./Login.styles.scss";
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userId: "",
+      userPwd: "",
+    };
+  }
+
+  handleIdPwd = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div className="Login">
+        <h3 className="login-title">로그인</h3>
+        <form className="login-form">
+          <input
+            className="id-input"
+            name="userId"
+            type="text"
+            placeholder="아이디를 입력해주세요"
+            onChange={this.handleIdPwd}
+          ></input>
+          <input
+            className="pwd-input"
+            name="userPwd"
+            type="text"
+            placeholder="비밀번호를 입력해주세요"
+            onChange={this.handleIdPwd}
+          ></input>
+          <div className="check-and-search">
+            <div className="check">
+              <label htmlFor="connect-type">
+                <input type="checkbox" name="connect" id="connect-type" />
+                보안접속
+              </label>
+            </div>
+            <div className="search">
+              <Link className="search-id">아이디 찾기</Link>
+              <span className="bar">|</span>
+              <Link className="search-pwd">비밀번호 찾기</Link>
+            </div>
+          </div>
+          <button className="loginButton">로그인</button>
+          <Link className="moveSingup">회원가입</Link>
+        </form>
+      </div>
+    );
   }
 }
 
--- src/Pages/Main/Main.component.js
+++ src/Pages/Main/Main.component.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Login from "../Login/Login.component";
 
 import "./Main.styles.scss";
 
 class Main extends Component {
   render() {
-    return <div>Hello BroKurly</div>;
+    return <Login />;
   }
 }
 
