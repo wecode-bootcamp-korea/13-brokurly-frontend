77c8ec2c384edbf6a01b153b7bf0cb690c4d4360
Soomyung Kang <vannskang@gmail.com>
2020-10-23 17:48:49 +0900

    add: modal start

--- src/Pages/ProductList/ProductList.component.js
+++ src/Pages/ProductList/ProductList.component.js
@@ -1,39 +1,46 @@
 import React, { Component } from "react";
-import { NavLink } from "react-router-dom";
+// import { NavLink } from "react-router-dom";
 
 import "./ProductList.styles.scss";
 import ProductCard from "./ProductCard/ProductCard.component";
 
 // Mock data
-const API = "http://localhost:3000/data/productlist/productlist_0.json";
+// const API = "http://localhost:3000/data/productlist/productlist_0.json";
 // Backend API
 // const API = "http://10.58.4.20:8000/product?productList=1&sort_type=0";
+const API = "http://10.58.4.20:8000/product?check=1&main=1&sub=1&sort_type=0";
 
 class ProductList extends Component {
   state = {
     isCategoryClick: true,
     isSortingClick: false,
-    categories: [
-      "전체보기",
-      "기본채소",
-      "쌈·샐러드·간편채소",
-      "브로콜리·특수채소",
-      "콩나물·버섯류",
-      "시금치·부추·나물",
-      "양파·마늘·생강·파",
-      "파프리카·피망·고추",
-    ],
-    products: [],
-    sortings: [],
     activeSorting: 0,
     activeCategory: 0,
   };
 
-  handleCategory = (e) => {
-    const { id } = e.target;
-    const { isCategoryClick } = this.state;
-    console.log(id);
-    this.setState({ isCategoryClick: !isCategoryClick, activeCategory: id });
+  handleCategory = async (e) => {
+    try {
+      const { id } = e.target;
+      const { activeSorting, isCategoryClick } = this.state;
+      let response;
+      +id !== 0
+        ? (response = await fetch(
+            `http://10.58.4.20:8000/product?check=0&main=1&sub=${id}&sort_type=${activeSorting}`
+          ))
+        : (response = await fetch(
+            `http://10.58.4.20:8000/product?check=1&main=1&sub=1&sort_type=${activeSorting}`
+          ));
+
+      if (response.state === 200) {
+        throw new Error("cannot fetch the data");
+      }
+      const { products } = await response.json();
+      this.setState({ products, activeCategory: id });
+    } catch (err) {
+      console.log("!! error alert !!");
+    }
+
+    // this.setState({ isCategoryClick: !isCategoryClick, activeCategory: id });
   };
 
   handleSorting = () => {
@@ -46,14 +53,21 @@ class ProductList extends Component {
   sortingList = async (e) => {
     try {
       const { id } = e.target;
+      const { activeCategory, isCategoryClick } = this.state;
       //local test
-      const response = await fetch(
-        `http://localhost:3000/data/productlist/productlist_${id}.json`
-      );
-      //backend API
       // const response = await fetch(
-      //   `http://10.58.4.20:8000/product?productList=1&sort_type=${id}`
+      //   `http://localhost:3000/data/productlist/productlist_${id}.json`
       // );
+      //backend API
+      let response;
+      activeCategory !== 0
+        ? (response = await fetch(
+            `http://10.58.4.20:8000/product?check=0&main=1&sub=${activeCategory}&sort_type=${id}`
+          ))
+        : (response = await fetch(
+            `http://10.58.4.20:8000/product?check=1&main=1&sub=1&sort_type=${id}`
+          ));
+
       if (response.status !== 200) {
         throw new Error("cannot fetch the data");
       }
@@ -70,8 +84,11 @@ class ProductList extends Component {
       if (response.status !== 200) {
         throw new Error("cannot fetch the data");
       }
-      const { products, available_sort } = await response.json();
-      this.setState({ products, sortings: available_sort });
+      const { products, categories, sortings } = await response.json();
+      // whole list handler
+      categories.unshift("전체보기");
+      console.log(categories);
+      this.setState({ products, categories, sortings });
     } catch (err) {
       console.log("!!error alert!!");
     }
@@ -83,10 +100,10 @@ class ProductList extends Component {
 
   render() {
     const {
-      isCategoryClick,
+      // isCategoryClick,
       isSortingClick,
-      categories,
       products,
+      categories,
       sortings,
       activeSorting,
       activeCategory,
@@ -101,10 +118,10 @@ class ProductList extends Component {
           </div>
           <div className="category-list">
             <ul>
-              {categories.length &&
+              {categories &&
                 categories.map((category, id) => (
-                  <li key={id} id={id}>
-                    {category}{" "}
+                  <li key={id} id={id} onClick={this.handleCategory}>
+                    {category}
                   </li>
                 ))}
             </ul>
@@ -112,7 +129,7 @@ class ProductList extends Component {
               className={`sort-trigger ${isSortingClick && "active-color"}`}
               onClick={this.handleSorting}
             >
-              <span>{sortings[activeSorting]}</span>
+              <span>{sortings && sortings[activeSorting]}</span>
               <i
                 className={`fas fa-chevron-${isSortingClick ? "up" : "down"}`}
               ></i>
@@ -122,18 +139,19 @@ class ProductList extends Component {
               onClick={this.handleSorting}
             >
               <ul>
-                {sortings.map((sortName, sortId) => (
-                  <li key={sortId} id={sortId} onClick={this.sortingList}>
-                    {sortName}
-                  </li>
-                ))}
+                {sortings &&
+                  sortings.map((sortName, sortId) => (
+                    <li key={sortId} id={sortId} onClick={this.sortingList}>
+                      {sortName}
+                    </li>
+                  ))}
               </ul>
             </div>
           </div>
         </div>
         <div className="card-list">
           <ul>
-            {products.length &&
+            {products &&
               products.map((product) => {
                 return <ProductCard product={product} key={product.id} />;
               })}
