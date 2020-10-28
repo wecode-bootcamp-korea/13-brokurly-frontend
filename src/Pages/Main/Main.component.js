acb1eb4f2f66242fde304a6e5ac76551cf0819bf
Jae Kim <9028364@gmail.com>
2020-10-29 03:22:35 +0900

    Modified Main for resolving conflict

--- src/Pages/Main/Main.component.js
+++ src/Pages/Main/Main.component.js
@@ -8,8 +8,22 @@ import {
   GET_FREQUENTLY_PRODUCT_API,
   GET_PURHCASE_LIST_API,
 } from "../../config";
+import MainBanner from "./MainBanner/MainBanner.component";
+import SideMenu from "../../Components/SideMenu/SideMenu.component";
+import SectionRender from "./SectionRender/SectionRender.component";
+
 import "./Main.styles.scss";
 class Main extends Component {
+  constructor() {
+    super();
+    this.state = {
+      specialSections: [],
+      scrollTop: 0,
+      // lastScrollTop: 0,
+      position: -2,
+    };
+    this.main = React.createRef();
+  }
   componentDidMount() {
     const {
       userToken,
@@ -17,6 +31,19 @@ class Main extends Component {
       getFrequentlyPurchaseItems,
       getPurchaseList,
     } = this.props;
+
+    fetch("http://localhost:3000/data/main/MainPageSectionsDataArr.json")
+      .then((res) => res.json())
+      .then((res) => {
+        this.setState({
+          specialSections: res.specialProductsSections,
+        });
+      })
+      .catch((error) => console.log(error.message));
+    const scrollTop = this.main.current.scrollTop;
+    this.setState({
+      scrollTop,
+    });
     fetch(GET_SHOPPINGBASKET_API, {
       headers: {
         "content-type": "application/json",
@@ -48,8 +75,81 @@ class Main extends Component {
       .then((purchaseList) => getPurchaseList(purchaseList))
       .catch((error) => console.log(error));
   }
+
+  onScrollDown = () => {
+    this.setState(
+      {
+        position: -1,
+      },
+      () => {
+        setTimeout(() => {
+          const scrollTop = this.main.current.scrollTop;
+          this.setState({ position: scrollTop > 5350 ? 2 : 0 });
+          setTimeout(() => {
+            this.setState({ scrollTop });
+          }, 200);
+        }, 200);
+      }
+    );
+  };
+
+  onScrollUp = () => {
+    this.setState(
+      {
+        position: 1,
+      },
+      () => {
+        setTimeout(() => {
+          const scrollTop = this.main.current.scrollTop;
+          this.setState({ position: scrollTop === 0 ? -2 : 0 });
+          setTimeout(() => {
+            this.setState({ scrollTop });
+          }, 200);
+        }, 200);
+      }
+    );
+  };
+
+  toggleSideMenuPosition = () => {
+    const scrollTop = this.main.current.scrollTop;
+    const posToDefaultCondition = scrollTop >= 240;
+    const posToInitialCondition = scrollTop < 240;
+    const posOnScrollDownCondition =
+      scrollTop >= 240 && scrollTop > this.state.scrollTop;
+    const posOnScrollUpCondition =
+      scrollTop >= 240 && scrollTop < this.state.scrollTop;
+    let position = -2;
+    this.setState({
+      scrollTop,
+    });
+    if (posToDefaultCondition) position = 0;
+    if (posToInitialCondition) position = -2;
+    if (posOnScrollDownCondition) {
+      this.onScrollDown();
+    } else if (posOnScrollUpCondition) {
+      this.onScrollUp();
+    } else {
+      this.setState({
+        position,
+      });
+    }
+  };
+
   render() {
-    return <div></div>;
+    const { specialSections, position } = this.state;
+    return (
+      <div
+        className="Main"
+        ref={this.main}
+        onScroll={this.toggleSideMenuPosition}
+      >
+        <MainBanner />
+        <SideMenu position={position} />
+        {specialSections.map((section) => (
+          <SectionRender section={section} key={section.id} />
+        ))}
+      </div>
+    );
   }
 }
 const mapStateToProps = ({ user }) => ({
