d443dc0fdde39c23ce440ab655e2cf87f7543b9c
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2020-10-29 00:30:28 +0900

    Add: add to gitignore

--- src/redux/user/user.types.js
+++
@@ -1,8 +0,0 @@
-const UserActionTypes = {
-  GET_CURRENT_USER: "GET_CURRENT_USER",
-  USER_LOGOUT: "USER_LOGOUT",
-  GET_TOKEN: "GET_TOKEN",
-  CLEAR_TOKEN: "CLEAR_TOKEN",
-};
-
-export default UserActionTypes;
