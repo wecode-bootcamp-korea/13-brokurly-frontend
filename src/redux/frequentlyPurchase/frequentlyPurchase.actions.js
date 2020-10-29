558eb342405764e71137be52020e227cd9930909
Jae Kim <9028364@gmail.com>
2020-10-29 22:47:39 +0900

    modified frequentPurchase.action

--- src/redux/frequentlyPurchase/frequentlyPurchase.actions.js
+++ src/redux/frequentlyPurchase/frequentlyPurchase.actions.js
@@ -1,9 +1,5 @@
 import FrequentlyPurchaseActionTypes from "./frequentlyPurhcase.types";
 
-// export const frequentlyPurchaseItemsBuyAll = () => ({
-//   type: FrequentlyPurchaseActionTypes.FREQUENTLY_PURCHASE_ITEMS_ORDER_ALL,
-// });
-
 export const clearFrequentlyPurchaseItemList = () => ({
   type: FrequentlyPurchaseActionTypes.CLEAR_FREQUENTLY_PURCHASE_ITEM_LIST,
 });
