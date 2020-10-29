81bc4d670e54f3a5ed94ed24d12a9b6b8728195f
Peter D Lee <dhlee9111@gmail.com>
2020-10-29 22:31:30 +0900

    Refactor: return to Main before adding fetch methods

--- src/Pages/Main/Main.component.js
+++ src/Pages/Main/Main.component.js
@@ -1,13 +1,4 @@
 import React, { Component } from "react";
-import { connect } from "react-redux";
-import { getCartItems } from "../../redux/cart/cart.actions";
-import { getFrequentlyPurchaseItems } from "../../redux/frequentlyPurchase/frequentlyPurchase.actions";
-import { getPurchaseList } from "../../redux/purchase/purchase.actions";
-import {
-  GET_SHOPPINGBASKET_API,
-  GET_FREQUENTLY_PRODUCT_API,
-  GET_PURHCASE_LIST_API,
-} from "../../config";
 import MainBanner from "./MainBanner/MainBanner.component";
 import SectionRender from "./SectionRender/SectionRender.component";
 import "./Main.styles.scss";
@@ -22,7 +13,6 @@ class Main extends Component {
 
   componentDidMount() {
     this.getAPIdata();
-    this.getUserAllData();
   }
 
   getAPIdata = async () => {
@@ -37,57 +27,6 @@ class Main extends Component {
     }
   };
 
-  getUserAllData = () => {
-    const {
-      userToken,
-      getCartItems,
-      getFrequentlyPurchaseItems,
-      getPurchaseList,
-    } = this.props;
-
-    fetch("http://localhost:3000/data/main/MainPageSectionsDataArr.json")
-      .then((res) => res.json())
-      .then((res) => {
-        this.setState({
-          specialSections: res.specialProductsSections,
-        });
-      })
-      .catch((error) => console.log(error.message));
-
-    fetch(GET_SHOPPINGBASKET_API, {
-      headers: {
-        "content-type": "application/json",
-        Authorization: userToken,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => data.shopping_list)
-      .then((cartItems) => getCartItems(cartItems))
-      .catch((error) => console.log(error));
-
-    fetch(GET_FREQUENTLY_PRODUCT_API, {
-      headers: {
-        "content-type": "application/json",
-        Authorization: userToken,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => data.product_list)
-      .then((product_list) => getFrequentlyPurchaseItems(product_list))
-      .catch((error) => console.log(error));
-
-    fetch(GET_PURHCASE_LIST_API, {
-      headers: {
-        "content-type": "application/json",
-        Authorization: userToken,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => data.order_list)
-      .then((purchaseList) => getPurchaseList(purchaseList))
-      .catch((error) => console.log(error));
-  };
-
   render() {
     const { specialSections } = this.state;
     return (
@@ -100,13 +39,5 @@ class Main extends Component {
     );
   }
 }
-const mapStateToProps = ({ user }) => ({
-  userToken: user.userToken,
-});
-const mapDispatchToProps = (dispatch) => ({
-  getCartItems: (cartItems) => dispatch(getCartItems(cartItems)),
-  getFrequentlyPurchaseItems: (purchaseItems) =>
-    dispatch(getFrequentlyPurchaseItems(purchaseItems)),
-  getPurchaseList: (purchaseItems) => dispatch(getPurchaseList(purchaseItems)),
-});
-export default connect(mapStateToProps, mapDispatchToProps)(Main);
+
+export default Main;
