0ea89822f56b8e06de65c01ef73869f4a1d5cc9f
deokyeong1020@gmail.com <deokyeong1020@gmail.com>
2020-11-01 14:34:49 +0900

    Fix : 장바구니 오류 수정

--- src/config.js
+++ src/config.js
@@ -1,37 +1,37 @@
 export const GET_SPECIAL_PRODUCTS =
-  "http://3.35.53.241:8000/products/home/md-choice?category=";
+  "http://3.35.175.82:8000/products/home/md-choice?category=";
 export const GET_SECTION_PRODUCTS =
-  "http://3.35.53.241:8000/products/home/section";
-export const GET_CATEGORY_API = "http://3.35.53.241:8000/products/category";
+  "http://3.35.175.82:8000/products/home/section";
+export const GET_CATEGORY_API = "http://3.35.175.82:8000/products/category";
 export const GET_SHOPPINGBASKET_API =
-  "http://3.35.53.241:8000/user/shoppingbasket";
+  "http://3.35.175.82:8000/user/shoppingbasket";
 export const SEND_RECENT_ITEM_SELECTED_API =
-  "http://3.35.53.241:8000/user/shoppingbasket-check";
-export const GET_CURRENT_USER_API = "http://3.35.53.241:8000/user/user-data";
+  "http://3.35.175.82:8000/user/shoppingbasket-check";
+export const GET_CURRENT_USER_API = "http://3.35.175.82:8000/user/user-data";
 export const GET_PURHCASE_LIST_API =
-  "http://3.35.53.241:8000/user/orderhistory";
+  "http://3.35.175.82:8000/user/orderhistory";
 export const GET_FREQUENTLY_PRODUCT_API =
-  "http://3.35.53.241:8000/user/frequentlyproduct";
+  "http://3.35.175.82:8000/user/frequentlyproduct";
 export const EARLY_DELIVERY_INFO =
   "https://res.kurly.com/pc/service/common/1908/delivery_190819.gif";
 export const CART = "https://res.kurly.com/pc/ico/1908/ico_cart_x2_v2.png";
 export const NEW_USER =
   "https://res.kurly.com/pc/service/common/1904/ico_new_20x20.png";
-export const PRODUCT_LIST = "http://3.35.53.241:8000/products";
-export const PRODUCT_REVIEW_LIST = "http://3.35.53.241:8000/user/product";
-export const PRODUCT_ITEM = "http://3.35.53.241:8000/products";
-export const PRODUCT_REVIEW_POST = "http://3.35.53.241:8000/user/user-review";
-export const PRODUCT_REVIEW = "http://3.35.53.241:8000/user/product";
+export const PRODUCT_LIST = "http://3.35.175.82:8000/products";
+export const PRODUCT_REVIEW_LIST = "http://3.35.175.82:8000/user/product";
+export const PRODUCT_ITEM = "http://3.35.175.82:8000/products";
+export const PRODUCT_REVIEW_POST = "http://3.35.175.82:8000/user/user-review";
+export const PRODUCT_REVIEW = "http://3.35.175.82:8000/user/product";
 export const PRODUCT_FAVORITE =
-  "http://3.35.53.241:8000/user/frequentlyproduct";
-export const USER_SEARCH_PASSWORD = "http://3.35.53.241:8000/user/findpassword";
-export const USER_SEARCH_ID = "http://3.35.53.241:8000/user/findid";
-export const SIGNIN = "http://3.35.53.241:8000/user/signin";
-export const SIGNUP = "http://3.35.53.241:8000/user/signup";
-export const SIGNUP_CHECK_ID = "http://3.35.53.241:8000/user/checkid";
-export const SIGNUP_CHECK_EMAIL = "http://3.35.53.241:8000/user/checkemail";
-export const SEND_SMS = "http://3.35.53.241:8000/user/sendsms";
-export const CHECK_SMS = "http://3.35.53.241:8000/user/checksms";
+  "http://3.35.175.82:8000/user/frequentlyproduct";
+export const USER_SEARCH_PASSWORD = "http://3.35.175.82:8000/user/findpassword";
+export const USER_SEARCH_ID = "http://3.35.175.82:8000/user/findid";
+export const SIGNIN = "http://3.35.175.82:8000/user/signin";
+export const SIGNUP = "http://3.35.175.82:8000/user/signup";
+export const SIGNUP_CHECK_ID = "http://3.35.175.82:8000/user/checkid";
+export const SIGNUP_CHECK_EMAIL = "http://3.35.175.82:8000/user/checkemail";
+export const SEND_SMS = "http://3.35.175.82:8000/user/sendsms";
+export const CHECK_SMS = "http://3.35.175.82:8000/user/checksms";
 
 export const LOGO_IMG =
   "https://images.velog.io/images/kho5420/post/121ee3b0-9ad9-43c5-87bb-23fb6c172f8a/image.png";
